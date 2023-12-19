6bb4879da4725664744c6adde3027b12d1c1a429
samuel farias <samuelcontatoe1@gmail.com>
2023-12-18 21:25:13 -0300

    exercicios concluidos, mas falta terminar a teoria

---
+++ js/app.js
@@ -0,0 +1,54 @@
+//exercice 1
+
+/*
+
+add7 = () => {
+    let number = 7;
+    let allnumber = parseInt(prompt());
+    let add7 = alert(parseInt(number + allnumber));
+
+}
+add7();
+
+*/
+
+/*
+
+// exercice 2
+
+multiply = () => {
+
+    let a = parseInt(prompt());
+    let b = parseInt(prompt());
+
+    return alert(a*b);
+
+}
+multiply();
+
+*/
+
+// exercice 3
+
+/*
+capitaliza = () => {
+    let string = prompt();
+    let uppString = string[0].toUpperCase();
+    let resultString = alert(uppString + string.substring(1)) ;
+}
+capitaliza();
+
+*/
+
+//exercice 4
+
+
+/*
+
+lastLetter = () => {
+    let string = prompt();
+    return alert(string.slice(-1));
+}
+lastLetter();
+
+*/
